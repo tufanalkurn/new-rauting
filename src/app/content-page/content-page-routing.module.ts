df1e48b5ee5f992392648a94c8f938970ca21a7d
Tufan Bisen <tufan.alkurn@gmail.com>
2016-12-21 23:02:04 +0530

    Project

---
+++ src/app/content-page/content-page-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ContentPageComponent } from './content-page.component';
+
+export const routes: Routes = [
+	{path: '', }
+];
+
+@NgModule({
+	imports: [],
+	exports: [],
+	providers: []
+})
+export class ContentRoutinModule {  }
