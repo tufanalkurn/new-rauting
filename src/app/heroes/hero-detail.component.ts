94231070698f0f1febf38aa01226664079760610
Tufan <tufan.alkurn@gmail.com>
2016-12-24 13:14:35 +0530

    Heros

--- src/app/heroes/hero-detail.component.ts
+++ src/app/heroes/hero-detail.component.ts
@@ -7,21 +7,7 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 import { Hero, HeroService }  from './hero.service';
 
 @Component({
-  template: `
-  <h2>HEROES</h2>
-  <div *ngIf="hero">
-    <h3>"{{ hero.name }}"</h3>
-    <div>
-      <label>Id: </label>{{ hero.id }}</div>
-    <div>
-      <label>Name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name"/>
-    </div>
-    <p>
-      <button>Back</button>
-    </p>
-  </div>
-  `,
+  templateUrl: "hero-detail.component.html",
 })
 export class HeroDetailComponent implements OnInit {
   
