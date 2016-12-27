b2bbfc2fe51aa1c96443e6c7bb48805f477cf231
Tufan <tufan.alkurn@gmail.com>
2016-12-27 17:43:11 +0530

    Services

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, HTTP_PROVIDERS } from '@angular/http';
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
+  viewProviders: [HTTP_PROVIDERS],
 })
 export class ServicesComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(http: Http) {
+    http.get('people.json')
+      // Call map on the response observable to get the parsed people object
+      .map(res => res.json())
+      // Subscribe to the observable to get the parsed people object and attach it to the
+      // component
+      .subscribe(people => this.people = people);
   }
+
+
 
 }
